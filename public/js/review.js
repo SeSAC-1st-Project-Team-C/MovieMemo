76bfd79fe66f4e722021733190920033f801a807
anemoa <anemoia.yves@gmail.com>
2024-07-10 18:57:40 +0900

    feat: 영화 상세 페이지 화면 구현

--- public/js/review.js
+++ public/js/review.js
@@ -3,3 +3,20 @@
 
 //리뷰 등록
 // 리뷰 등록 버튼 postReview()
+
+console.log('!!!');
+
+// const openBtn = document.querySelector('.button_open');
+// const closeBtn = document.querySelector('.button_close');
+const modal = document.querySelector('.movie_review_modal_bg');
+
+
+const postReview = () => {
+    modal.classList.remove('hidden');
+    modal.classList.add('visible');
+}
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    modal.classList.remove('visible');
+}
